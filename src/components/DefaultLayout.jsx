f4d00b11472a133f09968d75fd21c55d25dfe4c9
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-14 12:54:32 +0100

    delet footer

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 
 import Header from "./Header"
-import Footer from "./Footer"
 
 export default function DefaultLayout() {
 
@@ -9,7 +8,6 @@ export default function DefaultLayout() {
         <>
          <Header />
          <Outlet />
-         <Footer />
         </>
     )
 }
