2bb4f98b825287673acbbc34daba9fcca5b741fc
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-14 12:58:42 +0100

    add homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,16 @@ export default function HomePage() {
 
     return (
         <>
-        
+        <main>
+            <div className="container">
+                <div className="text-center mt-5">
+                    <h1 className="display-1 text-uppercase text-primary fw-bold">
+                        benvenuto
+                    </h1>
+
+                </div>
+            </div>
+        </main>
         </>
     )
 }
