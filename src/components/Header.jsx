b86bf57c2fb931d3e406cbe366f2258f4efd8a09
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-14 12:51:08 +0100

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import logo from "../assets/img/logo.jpeg"
+
 export default function Header() {
 
     return (
         <>
-          <header>
-            
+          <header className="p-3">
+            <div className="d-flex justify-content-between">
+                <div>
+                    <img src={logo} alt="Logo" />
+                </div>
+            </div>
           </header>
         </>
     )
