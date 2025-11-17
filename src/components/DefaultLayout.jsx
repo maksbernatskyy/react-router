2454a79c1e956c1bab5dbf43aaf3d97307ab3e15
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-17 11:55:53 +0100

    add main in default layout

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -7,7 +7,9 @@ export default function DefaultLayout() {
     return (
         <>
          <Header />
-         <Outlet />
+         <main>
+            <Outlet />
+         </main>
         </>
     )
 }
